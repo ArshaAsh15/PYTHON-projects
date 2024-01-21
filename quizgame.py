081fc13158dcb8045ed9854d7049e14590202588
Shanmugapriya <142212482+ArshaAsh15@users.noreply.github.com>
2024-01-21 20:52:04 +0530

    added code to my file

--- quizgame.py
+++ quizgame.py
@@ -1,2 +1,12 @@
 # my first project using python
 # QUIZ GAME
+print("welcome to my game space")
+
+playing = input("Do you want to play?  ")
+
+if playing != "yes":
+    quit()
+
+print("okay! Let's play ")
+
+
